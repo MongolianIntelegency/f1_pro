ddd59ca9d0fc08252087e7c0515a30e5f3ab069e
Kirill <kitrot07@gmail.com>
2021-10-25 12:57:06 +0300

    parcer result from BBC.com

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
 }
 
-req = requests.get(url, headers=headers)
-src = req.text
-print(src)
+full_page = requests.get(url, headers=headers)
+soup = BeautifulSoup(full_page.content, "html.parser")
+convert = soup.findAll("span", {"class": "gs-u-vh gs-u-display-inherit@m"})
+
+for child in convert: #get 
+    print(child.text)
+
+
+
+
+
+
+
